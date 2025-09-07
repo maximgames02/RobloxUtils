7a56b02ca6e09a46a8710e1a733de67e03e31f72
maximgames02 <103461154+maximgames02@users.noreply.github.com>
2025-09-07 07:25:23 +0300

    Update SimpleCreate.lua

--- SimpleCreate.lua
+++ SimpleCreate.lua
@@ -7,6 +7,7 @@ Create.E = function(eventName)
 end
 
 local objectsById = {}
+local pendingIdLinks = {}
 
 local function collectObjectsRecursive(item, parentId, list)
 	list = list or {}
@@ -29,13 +30,11 @@ local function collectObjectsRecursive(item, parentId, list)
 	return list
 end
 
-local function Create_PrivImpl(objectType)
-	if type(objectType) ~= 'string' then
-		error("Argument of Create must be a string", 2)
-	end
-
+local function Create_PrivImpl(DebugMode)
+	--
 	return function(dat)
 		objectsById = {}
+		pendingIdLinks = {}
 
 		local allObjects = collectObjectsRecursive(dat, nil)
 
@@ -48,22 +47,43 @@ local function Create_PrivImpl(objectType)
 			local data = entry.Data
 			local parent = nil
 			local ctor = nil
-			
+
 			pcall(function() obj = Instance.new(entry.Classname) end)
+
 			if obj ~= nil then
 				for k, v in pairs(data) do
 					if type(k) == 'string' then
 						if k == "Parent" then
 							parent = v
+						elseif k == "Tags" then
+							for _, tag in ipairs(v) do
+								game:GetService("CollectionService"):AddTag(obj, tostring(tag))
+							end
+						elseif k == "Attributes" then
+							for attrName, attrValue in pairs(v) do
+								obj:SetAttribute(attrName, attrValue)
+							end
 						elseif k == "Children" then
 							--
 						else
-							pcall(function() obj[k] = v end)
+							if type(v) == "string" and v:match("^PathID:%d+$") then
+								local id = tonumber(v:match("%d+"))
+								if id then
+									pendingIdLinks[obj] = pendingIdLinks[obj] or {}
+									pendingIdLinks[obj][k] = id
+								end
+							else
+								pcall(function()
+									obj[k] = v
+								end)
+							end
 						end
 					elseif type(k) == 'number' then
+						--
 					elseif type(k) == 'table' and k.__eventname then
 						if type(v) ~= 'function' then
-							error("Bad event entry: " .. tostring(k.__eventname))
+							error("Bad entry in Create body: Key `[Create.E\'"..k.__eventname.."\']` must have a function value\
+							got: "..tostring(v), 2)
 						end
 						pcall(function() obj[k.__eventname]:Connect(v) end)
 					elseif k == Create then
@@ -87,7 +107,9 @@ local function Create_PrivImpl(objectType)
 					ParentId = entry.ParentId,
 					ManualParent = parent
 				}
-				print(`Creating: [{entry.ID}] = {obj}`)
+				if DebugMode then
+					print(string.format("Creating: [%d] = %s", entry.ID, tostring(obj)))
+				end
 			end
 		end
 
@@ -102,6 +124,19 @@ local function Create_PrivImpl(objectType)
 			end
 		end
 
+		for obj, props in pairs(pendingIdLinks) do
+			for propName, refId in pairs(props) do
+				local ref = objectsById[refId]
+				if ref and ref.Object then
+					pcall(function()
+						obj[propName] = ref.Object
+					end)
+				else
+					warn("[Create] Объект с ID " .. tostring(refId) .. " не найден для свойства \"" .. tostring(propName) .. "\"")
+				end
+			end
+		end
+
 		return (objectsById[0] and objectsById[0].Object)
 	end
 end
@@ -112,8 +147,4 @@ setmetatable(Create, {
 	end
 })
 
-function Create.Init()
-	return Create
-end
-
-return Create.Init()
+return Create
